a0953303b56996239015b617e369c2b29099d186
Erkcurley <eric.curley94@gmail.com>
2017-01-16 18:58:11 -0500

    Beginning the movement commands

--- openComputers/excevate.lua
+++ openComputers/excevate.lua
@@ -2,27 +2,81 @@
 
 --New Excavate Program - Checks power level and returns home when low.
 
+local component = require("component")
+local computer = require("computer")
+local robot = require("robot")
+local shell = require("shell")
+local sides = require("sides")
 
-local function Move(x,y,z){
-	
-}
+local myX = 0
+local myY = 0
+local myZ = 0
+local face = 1
+local sides = 0
 
-local function Face(n){
+local function Move(x,y,z)
+	if x < myX then
+		Face("west")
+		while myX < x do 
+			robot.forward()
+		end
+	end
 	
-}
+	if z < myZ then
+		Face("north")
+		while myZ < z do 
+			robot.forward()
+		end
+	end 
+	
+ 	if y < myY then
+ 		while myY < y do
+ 			robot.down()
+		end
+ 	end
 
-local function Dig(){
+ 	if x > myX then
+ 		Face("east")
+		while myX > x do
+			robot.forward()
+		end
+	end
 	
-}
+	if z > myZ then
+		Face("west")
+		while myZ > z do 
+			robot.forward()
+		end
+	end
+	
+ 	if y > myY then
+ 		while myY > y do
+ 			robot.up()
+		end
+ 	end
+end
 
-local function Refuel(){
-	
-}
+local function Turn(n)
+	side = robot.sides()
 
-local function CheckInventory(){
-	
-}
+	while side < n do
+		robot.turnRight()
+	end
+end
 
-local function Home(){
+
+local function Dig()
 	
-}
+end
+
+local function Refuel()
+
+end
+
+local function CheckInventory()
+	
+end
+
+local function Home()
+	
+end
