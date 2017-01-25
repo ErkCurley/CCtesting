0e57e1cf74579c42f0c58d2222fe1e051d8f622e
Erkcurley <eric.curley94@gmail.com>
2017-01-25 11:50:55 -0500

    Created the core robot file and added required statements

---
+++ openComputers/Erkbot.lua
@@ -0,0 +1,9 @@
+-- Robot Self-tracking System created by Latias1290 for turtles. Ported to OpenComputers by Erkcurley
+
+local component = require("component")
+local computer = require("computer")
+local robot = require("robot")
+local shell = require("shell")
+local sides = require("sides")
+local io = require("io")
+
