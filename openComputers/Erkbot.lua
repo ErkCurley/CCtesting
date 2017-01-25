1826c0d68fa3a4c98faf56775df53585d6f06878
Erkcurley <eric.curley94@gmail.com>
2017-01-25 11:52:22 -0500

    Added read location from file

--- openComputers/Erkbot.lua
+++ openComputers/Erkbot.lua
@@ -10,3 +10,23 @@ local io = require("io")
 local xPos, yPos, zPos = nil
 local face = 1 --North = 0; West = 1; South = 2; East = 3;
 cal = false
+
+function readLocation()
+ local something = ""
+ local file = io.open("/home/location","r")
+ for line in file:lines() do
+  something = line
+  local arr = explode(",",line)
+  xPos = tonumber(arr[1])
+  yPos = tonumber(arr[2])
+  zPos = tonumber(arr[3])
+  face = tonumber(arr[4])
+  cal = true
+ end
+file:close()
+ 
+ --fs = io.open("/home/debug","w")
+ --fs:write(something)
+ --fs:close()
+ 
+end
