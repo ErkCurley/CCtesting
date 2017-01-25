2c1bc18c548a45779b8850b98e043b78cb988fc9
Eric <Eric.Curley94@gmail.com>
2017-01-25 00:21:48 -0500

    Added moveTo and Face functions

--- openComputers/moveTo.lua
+++ openComputers/moveTo.lua
@@ -11,9 +11,51 @@ local gps = require("gps")
 
 
 function moveTo(x,y,z)
+    gps.readLocation()
+    local xStart, yStart, zStart = getLocation()
 
-  xPos, yPos, zPos = 
-  
-  
-  
+    if xStart < x then
+        face(3) --Face Positive X
+        while xStart < x do
+            gps.forward()
+        end
+    end
+    if xStart > x then
+        face(1) --Face Negative X
+        while xStart > x do
+            gps.forward()
+        end
+    end
+    if zStart < z then
+        face(2) --Face Positive z
+        while zStart < z do
+            gps.forward()
+        end
+    end
+    if zStart > z then
+        face(0)    --Face Negative z
+        while zStart > z do
+            gps.forward()
+        end
+    end
+    if yStart < y then
+        while yStart < y do
+            gps.down()
+        end
+    end
+    if yStart > y then
+        while yStart < y do
+            gps.up()
+        end
+    end
+end
+
+function face(f)
+    gps.readLocation()
+    local fStart = gps.getFace()
+    
+    while fStart ~= f do
+        gps.faceRight()
+        fStart = gps.getFace()
+    end
 end
